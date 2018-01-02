73b91b400068ab7225d9b8d9c84185608552abf7
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-01-02 10:17:42 +0600

    GCD The Largest

---
+++ 12708.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<vector>
+#include<math.h>
+#include<string>
+#include<list>
+
+
+using namespace std;
+
+
+#define ll long long
+#define input scanf
+#define output printf 
+#define Loop while
+#define echo cout
+#define ret return
+#define MAX 999999999999999999
+#define MIN 0
+#define PI 3.1415
+
+int main(int argc, char** argv) {
+	
+	//freopen("c.txt","w",stdout);
+    int t;
+    cin>>t;
+    ll n;
+    while(t--)
+    {
+    	cin>>n;
+    	cout<<n/2<<endl;
+	}
+}
