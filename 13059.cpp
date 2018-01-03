ec17ac37061e589e95bb623c64c12b285fabbf9f
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-01-03 22:55:39 +0600

    Tennis Championship

---
+++ 13059.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<vector>
+#include<math.h>
+#include<string>
+#include<list>
+
+
+using namespace std;
+
+
+#define ll long long
+#define input scanf
+#define output printf 
+#define Loop while
+#define echo cout
+#define ret return
+#define MAX 999999999999999999
+#define MIN 0
+#define PI 3.1415
+
+int main(int argc, char** argv) {
+	
+	//freopen("c.txt","w",stdout);
+	ll n;
+	while(cin>>n)
+	{
+		printf("%lld\n",n-1);
+	}
+	return 0;
+}
