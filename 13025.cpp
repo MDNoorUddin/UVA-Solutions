8fe4ccaa95aa43ea4ebe00a6aed7aeaadae60143
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-01-03 13:00:09 +0600

    Back to the Past

---
+++ 13025.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<vector>
+#include<math.h>
+#include<string>
+#include<list>
+
+
+using namespace std;
+
+
+#define ll long long
+#define input scanf
+#define output printf 
+#define Loop while
+#define echo cout
+#define ret return
+#define MAX 999999999999999999
+#define MIN 0
+#define PI 3.1415
+
+int main(int argc, char** argv) {
+	
+	//freopen("c.txt","w",stdout);
+	printf("May 29, 2013 Wednesday\n");
+	return 0;
+}
