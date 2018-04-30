6d2d4b1c9b069699de9f3819d42ccf7b630323b5
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-05-01 00:31:25 +0600

    Airplane

---
+++ 12461.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+
+map<char,char> mp;
+
+int main(int argc, char** argv) {
+	  int n;
+	  while(cin>>n&&n)cout<<1<<"/"<<2<<endl;
+}
