9e2dc63d5f98ec14cabb0483b574a39251cf4230
MD. Noor Uddin <nooruddinneon16321@gmail.com>
2018-05-03 22:23:19 +0600

    Felipe and the Sequence

---
+++ 13187.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+#define g 9.81
+
+int main(int argc, char** argv) {
+	ll n;
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		cout<<((n+1)*(n+1))-1<<endl;
+	}
+}
