dca85605c343df2ac29b265ff6a43f66f0df4319
MD. Noor Uddin <nooruddinneon16321@gmail.com>
2018-05-03 23:20:14 +0600

    Squares, Lists and Digital Sums

---
+++ 13140.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+#define g 9.81
+
+int main(int argc, char** argv) {
+	printf("9999 99980001\n10000 100000000\n10001 100020001\n10002 100040004\n10003 100060009\n10004 100080016\n10005 100100025\n");
+}
